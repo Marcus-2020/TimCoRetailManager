dcb649cc07346a0adc90e24436011277b43305d1
Marcus Vinicius Menezes dos Santos <marcus.santos1808@hotmail.com>
2022-05-10 20:41:06 -0300

    Added WPF with Caliburn.Micro

---
+++ TRMDesktopUI/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TRMDesktopUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ TRMDesktopUI/Views/ShellView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TRMDesktopUI.Views
+{
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
