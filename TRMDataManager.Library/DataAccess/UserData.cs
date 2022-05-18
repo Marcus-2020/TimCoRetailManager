c4a209d36ff25c900f0ed0bfd454e08c48030281
Marcus Vinicius Menezes dos Santos <marcus.santos1808@hotmail.com>
2022-05-18 16:46:41 -0300

    feat: Added data access library for the API

---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public UserModel GetUserById(string id)
+        {
+            var sql = new SqlDataAccess();
+            
+            var p = new { Id = id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dto.spUserLookup", p, "TRMData");
+            
+            return output.SingleOrDefault();
+        }
+    }
+}
--- TRMDataStoredProceduresScript.sql
+++ TRMDataStoredProceduresScript.sql
@@ -1,42 +1,10 @@
-CREATE TABLE dto.[User] (
-    Id NVARCHAR(128) NOT NULL PRIMARY KEY,
-    FirstName NVARCHAR(50) NOT NULL,
-    LastName NVARCHAR(50) NOT NULL,
-    EmailAddress NVARCHAR(256) NOT NULL,
-    CreatedDate DATETIME2(7) NOT NULL DEFAULT  GETUTCDATE()
-);
+CREATE PROCEDURE  dto.[spUserlookup]
+    @Id nvarchar(128)
+AS
+BEGIN
+    set nocount on;
 
-CREATE TABLE dto.[Sale] (
-    Id INT IDENTITY NOT NULL PRIMARY KEY,
-    CashierId NVARCHAR(128) NOT NULL,
-    SaleDate DATETIME2(7) NOT NULL,
-    SubTotal MONEY NOT NULL,
-    Tax MONEY NOT NULL,
-    Total MONEY NOT NULL
-);
-
-CREATE TABLE dto.[SaleDetail] (
-    Id INT IDENTITY NOT NULL PRIMARY KEY,
-    SaleId INT NOT NULL,
-    ProductId INT NOT NULL,
-    Quantity int NOT NULL,
-    PurchasePrice MONEY NOT NULL,
-    Tax MONEY NOT NULL DEFAULT 0
-);
-
-CREATE TABLE dto.[Product] (
-    Id INT IDENTITY NOT NULL PRIMARY KEY,
-    ProductName NVARCHAR(100) NOT NULL,
-    Description NVARCHAR(MAX) NOT NULL,
-    RetailPrice MONEY NOT NULL,
-    CreatedDate DATETIME2(7) NOT NULL DEFAULT  GETUTCDATE(),
-    LastModified DATETIME2(7) NOT NULL DEFAULT  GETUTCDATE()
-);
-
-CREATE TABLE dto.[Inventory] (
-    Id INT IDENTITY NOT NULL PRIMARY KEY,
-    ProductId INT NOT NULL,
-    Quantity INT NOT NULL DEFAULT 1,
-    PurchasePrice MONEY NOT NULL,
-    PurchaseDate DATETIME2(7) NOT NULL DEFAULT  GETUTCDATE()
-);
+    select FirstName, LastName, EmailAddress
+    from dto.[User]
+    where Id = @Id;
+END
